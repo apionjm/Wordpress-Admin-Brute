6dffec976091bacfd81fd1d8cffae03949383bc7
Apion <113158128+apionjm@users.noreply.github.com>
2025-03-19 10:23:15 -0400

    Update wordpressbrute.py

--- wordpressbrute.py
+++ wordpressbrute.py
@@ -1,3 +1,16 @@
+#
+#     **             **                  
+#    ****    ****** //                   
+#   **//**  /**///** **  ******  ******* 
+#  **  //** /**  /**/** **////**//**///**
+# **********/****** /**/**   /** /**  /**
+#/**//////**/**///  /**/**   /** /**  /**
+#/**     /**/**     /**//******  ***  /**
+#//      // //      //  //////  ///   //
+
+
+#          Author: APION🔌
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
